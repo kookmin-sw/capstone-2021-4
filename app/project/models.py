139f1c4487af549d8f5991224a806498fd75cac4
jenny ruby <jenny@jennys-MacBook-Air.local>
2021-02-16 19:25:33 +0900

    보안그룹 스키마 작성 및 다른 테이플 스키마 추가

--- app/project/models.py
+++ app/project/models.py
@@ -1,10 +1,13 @@
-from project import db, bcrypt
+from project import app, db, bcrypt
 from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method
-from datetime import datetime
+import datetime
+from dataclasses import dataclass
+import jwt
 
 
 class User(db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(60), unique=True, nullable=False)
     _password = db.Column(db.Binary(60), nullable=False)
@@ -32,9 +35,9 @@ class User(db.Model):
         self.email_confirmation_sent_on = email_confirmation_sent_on
         self.email_confirmed = False
         self.email_confirmed_on = None
-        self.registered_on = datetime.now()
+        self.registered_on = datetime.datetime.now()
         self.last_logged_in = None
-        self.current_logged_in = datetime.now()
+        self.current_logged_in = datetime.datetime.now()
         self.role = role
 
     @hybrid_property
@@ -76,7 +79,31 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.email)
-  
+
+    def encode_auth_token(self, user_id):
+        """
+        Generates the Auth Token
+        :return: string
+        """
+        
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+
+            # print("SECRET KEY{}".format(app.config.get("SECRET_KEY")))
+            return jwt.encode(
+                payload,
+                app.config.get("SECRET_KEY"),
+                algorithm='HS256'
+            )
+        except Exception as e:
+            print("Error")
+            return e
+
+    
 
 
 class Items(db.Model):
@@ -93,9 +120,13 @@ class Items(db.Model):
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
-
+@dataclass
 class Oslist(db.Model): # 제공 os
     __tablename__ = 'oslist'
+    id: int
+    os_name: str
+    aws_image_id: str
+
     id = db.Column(db.Integer, primary_key=True)
     os_name = db.Column(db.String(30), nullable=False)
     aws_image_id = db.Column(db.String(30), nullable=False)
@@ -110,15 +141,30 @@ class Oslist(db.Model): # 제공 os
         self.aws_image_id = aws_image_id
     @property
     def serialize(self):
-       return {c.name: unicode(getattr(self, c.name)) for c in self.__table__.columns}
+        return {
+            'id': self.id,
+            'os_name': self.os_name,
+            'aws_image_id': self.aws_image_id,
+        }
 
 def dump_datetime(value):
     """Deserialize datetime object into string form for JSON processing."""
     if value is None:
         return None
     return [value.strftime("%Y-%m-%d"), value.strftime("%H:%M:%S")]
+
+@dataclass
 class Plan(db.Model):
     __tablename__ = 'plan'
+    id: int
+    plan_name: str
+    aws_plan: str
+    core: int
+    ram: int
+    traffic:int
+    ssd: int
+    iops: int
+
     id = db.Column(db.Integer, primary_key=True)
     plan_name = db.Column(db.String(30), nullable=False)
     aws_plan = db.Column(db.String(30), nullable=False)
@@ -144,9 +190,16 @@ class Plan(db.Model):
     @property
     def serialize(self):
        """Return object data in easily serializable format"""
-       return [
-           self.id
-       ]
+       return {
+            'id': self.id,
+            'plan_name': self.plan_name,
+            'aws_plan': self.aws_plan,
+            'core': self.core,
+            'ram': self.ram,
+            'traffic': self.traffic,
+            'ssd': self.ssd,
+            'iops': self.iops, 
+        }
             
         
 
@@ -175,39 +228,71 @@ class NetInterface(db.Model):
     interface_id = db.Column(db.String(30), nullable=False)
     subnet_id = db.Column(db.Integer, db.ForeignKey("subnets.id"))
     cloud_id = db.Column(db.Integer, nullable=True) # cloud id
-    
+    attached_at = db.Column(db.DateTime, nullable=True)
+    detached_at = db.Column(db.DateTime, nullable=True)
+    deleted_at = db.Column(db.DateTime, nullable=True)
 
-    def __init__(self, interface_id, subnet_id, cloud_id=None): 
+    def __init__(self, interface_id, subnet_id, cloud_id, attached_at=None): 
         self.cloud_id = cloud_id
         self.interface_id = interface_id
         self.subnet_id = subnet_id
+        self.attached_at = attached_at
+        
     @property
     def as_dict(self):
        return {c.name: unicode(getattr(self, c.name)) for c in self.__table__.columns}
         
 
-class SecurityRule(db.Model):
-    __tablename__ = 'securityrule'
+class SecurityGroup(db.Model):
+    __tablename__ = 'securitygroup'
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
     sec_group_id = db.Column(db.String(30), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     associated_to = db.Column(db.Integer, nullable=True) # cloud.id
-    def __init__(self, sec_group_id, user_id, associated_to=None):
+    vpc_id = db.Column(db.Integer, db.ForeignKey("user_vpc.id"))
+
+    def __init__(self, name, sec_group_id, user_id, associated_to, vpc_id):
+        self.name = name
         self.sec_group_id = sec_group_id
         self.user_id = user_id
         self.associated_to = associated_to
+        self.vpc_id = vpc_id
+
     @property
     def as_dict(self):
        return {c.name: unicode(getattr(self, c.name)) for c in self.__table__.columns}
 
+class SecurityRule(db.Model):
+    __tablename__ = 'securityrule'
+    id = db.Column(db.Integer, primary_key=True) 
+    protocol= db.Column(db.String(10), nullable=False)
+    fromport = db.Column(db.Integer, nullable=False)
+    toport = db.Column(db.Integer, nullable=False)
+    cidr = db.Column(db.String(20), nullable=False)
+    desc = db.Column(db.String(30), nullable=True)
+    group_id = db.Column(db.Integer, db.ForeignKey("securitygroup.id"))
+    
+    def __init__(self, protocol, fromport, toport, cidr, desc, group_id):
+        self.protocol = protocol
+        self.fromport = fromport
+        self.toport = toport
+        self.cidr = cidr
+        self.desc = desc
+        self.group_id = group_id
         
 
-
+@dataclass
 class Subnet(db.Model):
     __tablename__ = 'subnets'
+    id: int
+    subnet_id: str
+    cidr_block_ipv4: str 
+
     id = db.Column(db.Integer, primary_key=True)
     subnet_id = db.Column(db.String(30), nullable=False)
     cidr_block_ipv4 = db.Column(db.String(24))
+    
     
 
     def __init__(self, subnet_id, cidr_block_ipv4):
@@ -216,14 +301,20 @@ class Subnet(db.Model):
     @property
     def as_dict(self):
        return {c.name: unicode(getattr(self, c.name)) for c in self.__table__.columns}
-
+@dataclass
 class Keypair(db.Model): #for connector
     __tablename__ = 'keypair'
+    id: int
+    name: str
+    fingerprint: str
+    keyid: str
+    user_id: str
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
     fingerprint = db.Column(db.String(59), nullable=True)
     keyid = db.Column(db.String(30), nullable=False)
-    user_id = db.Column(db.String(30), nullable=False)
+    user_id = db.Column(db.Integer , nullable=False)
 
     def __init__(self, name, fingerprint, keyid, user_id):
         self.name = name
@@ -234,9 +325,23 @@ class Keypair(db.Model): #for connector
     def as_dict(self):
        return {c.name: unicode(getattr(self, c.name)) for c in self.__table__.columns}
 
-
+@dataclass
 class Cloud(db.Model):
     __tablename__ = 'cloud'
+    id: int
+    hostname: str
+    plan_id: int
+    user_id: int
+    os: str
+    status: str
+    ip_addr: str
+    region: str
+    created_at: str
+    deleted_at: str
+    keypair_id: str
+    vpc_id: str
+    aws_instance_id: str
+
     id = db.Column(db.Integer, primary_key=True)
     hostname = db.Column(db.String(30), nullable=False)
     plan_id = db.Column(db.Integer, db.ForeignKey('plan.id'))
@@ -264,14 +369,35 @@ class Cloud(db.Model):
         self.status = status
         self.ip_addr = ip_addr
         self.region = region
-        self.created_at = datetime.now()
+        self.created_at = datetime.datetime.now()
         self.keypair_id = keypair_id
         self.vpc_id = vpc_id
         self.aws_instance_id = aws_instance_id
 
-    @property
+    
+    
     def as_dict(self):
        return {c.name: unicode(getattr(self, c.name)) for c in self.__table__.columns}
+
+
+class BlacklistToken(db.Model):
+    """
+    Token Model for storing JWT tokens
+    """
+    __tablename__ = 'blacklist_tokens'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    token = db.Column(db.String(500), unique=True, nullable=False)
+    blacklisted_on = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, token):
+        self.token = token
+        self.blacklisted_on = datetime.datetime.now()
+
+    def __repr__(self):
+        return '<id: token: {}'.format(self.token)
+
+ 
 
 # class Billing(db.Model):
 #     __tablename__ = 'billing'
