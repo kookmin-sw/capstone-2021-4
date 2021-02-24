a891b73a87de43e55aa20983233214f66bf5551a
parkhuijoo <gmlwngmlwn@kookmin.ac.kr>
2021-02-24 18:24:31 +0900

    security,setting init

--- app/js/cloud_add.js
+++ app/js/cloud_add.js
@@ -4,20 +4,3 @@ cloud_add.create = function() {
     route("cloud_list")
 }
 
-
-// focusMethod = function getFocus() {
-    // document.getElementById("server_name").scrollIntoView(true);
-    // document.getElementById("server_name").focus({preventScroll:false});
-    // document.getElementsByClassName("card")[0].style.background= "#31ba850e";
-    // document.getElementsByClassName("card")[0].style.background="white";
-    // document.getElementsByClassName("card")[0].style.background="white";
-    // document.getElementsByClassName("card")[0].style.background="white";
-    // document.getElementById("select_btn1").style.backgroundColor = "#31ba85";
-    // document.getElementById("select_btn1").style.color = "white";
-    // document.getElementById("select_btn2").style.backgroundColor = "white";
-    // document.getElementById("select_btn2").style.color = "#31ba85";
-    // document.getElementById("select_btn3").style.backgroundColor = "white";
-    // document.getElementById("select_btn3").style.color = "#31ba85";
-    // document.getElementById("select_btn4").style.backgroundColor = "white";
-    // document.getElementById("select_btn4").style.color = "#31ba85";
-// }
--- app/js/price_view.js
+++ app/js/price_view.js
@@ -11,3 +11,43 @@ price_view.test_function2 = function(a,b) {
 price_view.move_view = function() {
     route('cloud_list') // 클라우드  리스트로 화면 이동
 }
+
+
+// switchcontents
+
+var tabButtons=document.querySelectorAll(".tabContainer .btn_group button");
+var tabPanels=document.querySelectorAll(".tabContainer  .tabPanel");
+
+function showPanel(panelIndex,colorCode) {
+    tabButtons.forEach(function(node){
+        node.style.backgroundColor="";
+        node.style.color="";
+    });
+    tabButtons[panelIndex].style.backgroundColor=colorCode;
+    tabButtons[panelIndex].style.color="white";
+    tabPanels.forEach(function(node){
+        node.style.display="none";
+    });
+    tabPanels[panelIndex].style.display="block";
+    tabPanels[panelIndex].style.backgroundColor="white";
+}
+
+  
+
+
+
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".options-container");
+
+const optionsList = document.querySelectorAll(".option");
+
+selected.addEventListener("click", () => {
+  optionsContainer.classList.toggle("active");
+});
+
+optionsList.forEach(o => {
+  o.addEventListener("click", () => {
+    selected.innerHTML = o.querySelector("label").innerHTML;
+    optionsContainer.classList.remove("active");
+  });
+});
