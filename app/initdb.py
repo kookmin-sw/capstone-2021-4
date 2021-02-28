6b4a649593865f4b58a702b98eae8af8bb892ce7
jenny ruby <jenny@jennys-MacBook-Air.local>
2021-02-28 16:04:21 +0900

    샘플 플랜 추가(트래픽 많은것)

--- app/initdb.py
+++ app/initdb.py
@@ -1,7 +1,7 @@
 from project import app,db
 from project.models import Oslist, Plan
 
-sample_plan = Plan('m1.small', 't3.small', 1, 1024, 1, 40, 4000)
+sample_plan = Plan('m2.small', 't3.small', 1, 1024, 2048, 40, 4000)
 db.session.add(sample_plan)
 
 sample_os = Oslist('ubuntu20.04', 'ami-067abcae434ee508b')
