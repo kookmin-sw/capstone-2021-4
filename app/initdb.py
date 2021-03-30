7bec05d40deeeff84381299c637c39d06df61e92
kimJenny <qsa@kakao.com>
2021-03-30 07:47:15 +0000

    plan 추가 스크립트 수정 , admin 계정 추가 스크립트

--- app/initdb.py
+++ app/initdb.py
@@ -1,5 +1,5 @@
 from project import app,db
-from project.models import Oslist, Plan
+from project.models import Oslist, Plan, User
 #plan
 # id: int
 # plan_name: str
@@ -9,27 +9,34 @@ from project.models import Oslist, Plan
 # traffic:int
 # ssd: int
 # iops: int
-# sample_plan = Plan('m2.small', 't3.small', 1, 2048, 2000, 40, 4000)
+def plan_add(): 
+    sample_plan = Plan('m2.small', 't3.small', 1, 2048, 2000, 40, 4000)
+    sample_plan2 = Plan('m3.medium', 't3.medium', 4096, 4000, 2048, 60, 4500)
+    sample_plan3 = Plan('m3.big', 't3.xlarge', 4, 16384, 6000, 100, 5000)
+    sample_plan4 = Plan('m4.large', 't3.2xlarge', 8, 32768, 8000, 100, 5500)
+    db.session.add(sample_plan)
+    db.session.add(sample_plan2)
+    db.session.add(sample_plan3)
+    db.session.add(sample_plan4)
+    db.session.commit()
 
-sample_plan2 = Plan('m3.medium', 't3.medium	', 4096, 4000, 2048, 60, 4500)
-sample_plan3 = Plan('m3.big', 't3.xlarge', 4, 16384, 6000, 100, 5000)
-sample_plan4 = Plan('m4.large', 't3.2xlarge', 8, 32768, 8000, 100, 5500)
-db.session.add(sample_plan2)
-db.session.add(sample_plan3)
-db.session.add(sample_plan4)
 
-
+def admin_add():
 # Admin User ad
-# new_user = User('admin@testtest.com', '123!asdf')
-# new_user.authenticated = True
-# new_user.role ='admin'
+    new_user = User('admin@testtest.com', '123!asdf')
+    new_user.authenticated = True
+    new_user.role ='admin'
+    
+    db.session.add(new_user)
 
-# db.session.add(new_user)
+def os_add():
+    sample_os = Oslist('ubuntu20.04', 'ami-067abcae434ee508b')
+    sample_os2  = Oslist('amazonLinux', 'ami-09282971cf2faa4c9')
+    
+    db.session.add(sample_os)
+    db.session.add(sample_os2)
+    
+    db.session.commit()
 
-# sample_os = Oslist('ubuntu20.04', 'ami-067abcae434ee508b')
-# sample_os2  = Oslist('amazonLinux', 'ami-09282971cf2faa4c9')
 
-# db.session.add(sample_os)
-# db.session.add(sample_os2)
-
-db.session.commit()
+plan_add()
